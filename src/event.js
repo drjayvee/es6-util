35637247284f268fc03c9c089aaa6fa0a9d0ee1f
Jeroen Versteeg <jeroen@asystance.nl>
2015-06-23 16:18:04 +0200

    reformatted ES6 code (removed leading tab)

--- src/event.js
+++ src/event.js
@@ -1,293 +1,293 @@
 /*jshint esnext:true*/
 	
-	// region DispatchFacade
-	function DispatchFacade () {
-		this._eventNode = document.createElement('a');
-	}
+// region DispatchFacade
+function DispatchFacade () {
+	this._eventNode = document.createElement('a');
+}
+
+DispatchFacade.prototype = {
+	constructor: DispatchFacade,
 	
-	DispatchFacade.prototype = {
-		constructor: DispatchFacade,
-		
-		attach: function (type, fn) {
-			this._eventNode.addEventListener(type, fn);
-		},
-		
-		detach: function (type, fn) {
-			this._eventNode.removeEventListener(type, fn, false);
-		},
-
-		/**
-		 * 
-		 * @param {String|Event} typeOrEvent
-		 * @param {Boolean} [preventable=true]
-		 * @param {Object} [data]
-		 * @returns {boolean}
-		 */
-		dispatch: function (typeOrEvent, preventable, data) {
-			var e = typeOrEvent;
-			
-			if (typeof typeOrEvent === 'string') {
-				e = this.createEvent(typeOrEvent, preventable, data);
-			}
-			
-			return this._eventNode.dispatchEvent(e);
-		},
-
-		/**
-		 * 
-		 * @param {string} type
-		 * @param {boolean} [preventable=true]
-		 * @param {Object} [data]
-		 * @returns {Event}
-		 */
-		createEvent: function (type, preventable, data) {
-			var e = document.createEvent('Event');
-			e.initEvent(type, false, preventable !== false);
-
-			if (data) {
-				Object.keys(data).forEach(function ( key ) {
-					if (e[key]) {		// don't overwrite "native" event properties
-						return;
-					}
-					e[key] = data[key];
-				});
-			}
-			
-			return e;
-		}
-	};
-	// endregion
+	attach: function (type, fn) {
+		this._eventNode.addEventListener(type, fn);
+	},
 	
-	// region Subscription
+	detach: function (type, fn) {
+		this._eventNode.removeEventListener(type, fn, false);
+	},
+
 	/**
 	 * 
-	 * @param	{DispatchFacade} dispatch
+	 * @param {String|Event} typeOrEvent
+	 * @param {Boolean} [preventable=true]
+	 * @param {Object} [data]
+	 * @returns {boolean}
+	 */
+	dispatch: function (typeOrEvent, preventable, data) {
+		var e = typeOrEvent;
+		
+		if (typeof typeOrEvent === 'string') {
+			e = this.createEvent(typeOrEvent, preventable, data);
+		}
+		
+		return this._eventNode.dispatchEvent(e);
+	},
+
+	/**
+	 * 
+	 * @param {string} type
+	 * @param {boolean} [preventable=true]
+	 * @param {Object} [data]
+	 * @returns {Event}
+	 */
+	createEvent: function (type, preventable, data) {
+		var e = document.createEvent('Event');
+		e.initEvent(type, false, preventable !== false);
+
+		if (data) {
+			Object.keys(data).forEach(function ( key ) {
+				if (e[key]) {		// don't overwrite "native" event properties
+					return;
+				}
+				e[key] = data[key];
+			});
+		}
+		
+		return e;
+	}
+};
+// endregion
+
+// region Subscription
+/**
+ * 
+ * @param	{DispatchFacade} dispatch
+ * @param	{String} type
+ * @param	{Function} callback
+ * @param	{Object} [context]
+ * @param	{Boolean} [once=false]
+ * @constructor
+ */
+function Subscription (dispatch, type, callback, context, once) {
+	var self = this;
+	
+	this.dispatch = dispatch;
+	this.type = type;
+	this.callback = callback;
+	this.context = context;
+	
+	this._eventFn = function (e) {	// create a unique function so detach will only remove this sub
+		callback.call(context, e);
+		if (once) {
+			self.detach();
+		}
+	};
+}
+
+Subscription.prototype = {
+	constructor: Subscription,
+	
+	attach: function () {
+		this.dispatch.attach(this.type, this._eventFn);
+	},
+	
+	detach: function () {
+		this.dispatch.detach(this.type, this._eventFn);
+	}
+};
+// endregion
+
+// region EventTarget
+export function EventTarget () {
+	this._eventDispatch = new DispatchFacade();
+	this._eventDefinitions = {};
+	this._eventSubs = {};
+}
+
+EventTarget.defaultConfig = {
+	preventable:	true,
+	preventedFn:	null,
+	defaultFn:		null
+};
+
+EventTarget.prototype = {
+	constructor: EventTarget,
+
+	/**
+	 * Define a new event
+	 * 
+	 * @param	{String} type
+	 * @param	{Object} config
+	 */
+	publish: function (type, config) {
+		var cfg;
+		
+		if (this._eventDefinitions[type]) {
+			throw new Error('Event "' + type + '" has already been published');
+		}
+		
+		// merge config with default config
+		cfg = {};
+		Object.keys(EventTarget.defaultConfig).forEach(function (key) {
+			cfg[key] = config.hasOwnProperty(key) ? config[key] : EventTarget.defaultConfig[key];
+		});
+		
+		this._eventDefinitions[type] = cfg;
+	},
+	
+	/**
+	 * 
 	 * @param	{String} type
 	 * @param	{Function} callback
-	 * @param	{Object} [context]
-	 * @param	{Boolean} [once=false]
-	 * @constructor
+	 * @param	{Object} [context]		if not specified, use this
+	 * @returns {Subscription}
 	 */
-	function Subscription (dispatch, type, callback, context, once) {
-		var self = this;
-		
-		this.dispatch = dispatch;
-		this.type = type;
-		this.callback = callback;
-		this.context = context;
-		
-		this._eventFn = function (e) {	// create a unique function so detach will only remove this sub
-			callback.call(context, e);
-			if (once) {
-				self.detach();
-			}
-		};
-	}
+	on: function (type, callback, context) {
+		return this._on(type, callback, context);
+	},
 	
-	Subscription.prototype = {
-		constructor: Subscription,
+	/**
+	 * @see on
+	 */
+	once: function (type, callback, context) {
+		return this._on(type, callback, context, true);
+	},
+	
+	/**
+	 * @see on
+	 */
+	after: function (type, callback, context) {
+		return this.on('AFTER:' + type, callback, context);
+	},
+	
+	/**
+	 * @see on
+	 */
+	onceAfter: function (type, callback, context) {
+		return this.once('AFTER:' + type, callback, context);
+	},
+
+	/**
+	 * Remove all subscriptions for this type (on and after), callback, context
+	 * 
+	 * @param	{String} type
+	 * @param	{Function} callback
+	 * @param	{Object} [context]		if not specified, use this
+	 */
+	detach: function (type, callback, context) {
+		var subs;
 		
-		attach: function () {
-			this.dispatch.attach(this.type, this._eventFn);
-		},
+		context = context || this;
 		
-		detach: function () {
-			this.dispatch.detach(this.type, this._eventFn);
+		// merge on and after subs
+		subs = this._findSubs(type, callback, context).concat(
+			this._findSubs('AFTER:' + type, callback, context)
+		);
+		
+		subs.forEach(function (sub) {
+			sub.detach();
+			this._deleteSub(sub);
+		}, this);
+	},
+
+	/**
+	 * @param	{String} type
+	 * @param	{Function} callback
+	 * @param	{Object} [context=this]
+	 * @param	{Boolean} [once=false]
+	 * @returns {Subscription}
+	 * @private
+	 */
+	_on: function (type, callback, context, once) {
+		var sub;
+		
+		context = context || this;
+		
+		// prevent duplicate subs
+		sub = this._findSubs(type, callback, context);
+		if (sub.length === 1) {
+			return sub[0];
 		}
-	};
-	// endregion
-	
-	// region EventTarget
-	export function EventTarget () {
-		this._eventDispatch = new DispatchFacade();
-		this._eventDefinitions = {};
-		this._eventSubs = {};
-	}
-	
-	EventTarget.defaultConfig = {
-		preventable:	true,
-		preventedFn:	null,
-		defaultFn:		null
-	};
-	
-	EventTarget.prototype = {
-		constructor: EventTarget,
-
-		/**
-		 * Define a new event
-		 * 
-		 * @param	{String} type
-		 * @param	{Object} config
-		 */
-		publish: function (type, config) {
-			var cfg;
-			
-			if (this._eventDefinitions[type]) {
-				throw new Error('Event "' + type + '" has already been published');
-			}
-			
-			// merge config with default config
-			cfg = {};
-			Object.keys(EventTarget.defaultConfig).forEach(function (key) {
-				cfg[key] = config.hasOwnProperty(key) ? config[key] : EventTarget.defaultConfig[key];
-			});
-			
-			this._eventDefinitions[type] = cfg;
-		},
 		
-		/**
-		 * 
-		 * @param	{String} type
-		 * @param	{Function} callback
-		 * @param	{Object} [context]		if not specified, use this
-		 * @returns {Subscription}
-		 */
-		on: function (type, callback, context) {
-			return this._on(type, callback, context);
-		},
+		// create new sub
+		sub = new Subscription(
+			this._eventDispatch,
+			type,
+			callback,
+			context,
+			once === true
+		);
 		
-		/**
-		 * @see on
-		 */
-		once: function (type, callback, context) {
-			return this._on(type, callback, context, true);
-		},
+		sub.attach();
 		
-		/**
-		 * @see on
-		 */
-		after: function (type, callback, context) {
-			return this.on('AFTER:' + type, callback, context);
-		},
+		if (!this._eventSubs[type]) {
+			this._eventSubs[type] = [];
+		}
+		this._eventSubs[type].push(sub);
 		
-		/**
-		 * @see on
-		 */
-		onceAfter: function (type, callback, context) {
-			return this.once('AFTER:' + type, callback, context);
-		},
+		return sub;
+	},
 
-		/**
-		 * Remove all subscriptions for this type (on and after), callback, context
-		 * 
-		 * @param	{String} type
-		 * @param	{Function} callback
-		 * @param	{Object} [context]		if not specified, use this
-		 */
-		detach: function (type, callback, context) {
-			var subs;
-			
-			context = context || this;
-			
-			// merge on and after subs
-			subs = this._findSubs(type, callback, context).concat(
-				this._findSubs('AFTER:' + type, callback, context)
-			);
-			
-			subs.forEach(function (sub) {
-				sub.detach();
-				this._deleteSub(sub);
-			}, this);
-		},
-
-		/**
-		 * @param	{String} type
-		 * @param	{Function} callback
-		 * @param	{Object} [context=this]
-		 * @param	{Boolean} [once=false]
-		 * @returns {Subscription}
-		 * @private
-		 */
-		_on: function (type, callback, context, once) {
-			var sub;
-			
-			context = context || this;
-			
-			// prevent duplicate subs
-			sub = this._findSubs(type, callback, context);
-			if (sub.length === 1) {
-				return sub[0];
-			}
-			
-			// create new sub
-			sub = new Subscription(
-				this._eventDispatch,
-				type,
-				callback,
-				context,
-				once === true
-			);
-			
-			sub.attach();
-			
-			if (!this._eventSubs[type]) {
-				this._eventSubs[type] = [];
-			}
-			this._eventSubs[type].push(sub);
-			
-			return sub;
-		},
-
-		/**
-		 * @param	{String} type
-		 * @param	{Function} callback
-		 * @param	{Object} [context=this]
-		 * @returns {Subscription[]}
-		 * @private
-		 */
-		_findSubs: function (type, callback, context) {
-			var i,
-				found = [],
-				sub,
-				subs = this._eventSubs[type];
-			
-			if (!subs) {
-				return found;
-			}
-			
-			for (i = 0; i < subs.length; i += 1) {
-				sub = subs[i];
-				if (sub.callback === callback && sub.context === context) {
-					found.push(sub);
-				}
-			}
+	/**
+	 * @param	{String} type
+	 * @param	{Function} callback
+	 * @param	{Object} [context=this]
+	 * @returns {Subscription[]}
+	 * @private
+	 */
+	_findSubs: function (type, callback, context) {
+		var i,
+			found = [],
+			sub,
+			subs = this._eventSubs[type];
+		
+		if (!subs) {
 			return found;
-		},
-
-		/**
-		 * @param	{Subscription} sub
-		 * @private
-		 */
-		_deleteSub: function (sub) {
-			this._eventSubs[sub.type].splice(
-				this._eventSubs[sub.type].indexOf(sub),
-				1
-			);
-		},
+		}
 		
-		/**
-		 * 
-		 * @param {String} type
-		 * @param {Object} [data]
-		 * @return {boolean}	true if event was not cancelled
-		 */
-		fire: function (type, data) {
-			var success,
-				def = this._eventDefinitions[type] || EventTarget.defaultConfig;
-			
-			success = this._eventDispatch.dispatch(type, def.preventable, data);
-			
-			if (success) {
-				if (def.defaultFn) {
-					def.defaultFn(data);
-				}
-				
-				this._eventDispatch.dispatch('AFTER:' + type, false, data);
-			} else if (def.preventedFn) {
-				def.preventedFn(data);
+		for (i = 0; i < subs.length; i += 1) {
+			sub = subs[i];
+			if (sub.callback === callback && sub.context === context) {
+				found.push(sub);
+			}
+		}
+		return found;
+	},
+
+	/**
+	 * @param	{Subscription} sub
+	 * @private
+	 */
+	_deleteSub: function (sub) {
+		this._eventSubs[sub.type].splice(
+			this._eventSubs[sub.type].indexOf(sub),
+			1
+		);
+	},
+	
+	/**
+	 * 
+	 * @param {String} type
+	 * @param {Object} [data]
+	 * @return {boolean}	true if event was not cancelled
+	 */
+	fire: function (type, data) {
+		var success,
+			def = this._eventDefinitions[type] || EventTarget.defaultConfig;
+		
+		success = this._eventDispatch.dispatch(type, def.preventable, data);
+		
+		if (success) {
+			if (def.defaultFn) {
+				def.defaultFn(data);
 			}
 			
-			return success;
+			this._eventDispatch.dispatch('AFTER:' + type, false, data);
+		} else if (def.preventedFn) {
+			def.preventedFn(data);
 		}
-	};
-	// endregion
+		
+		return success;
+	}
+};
+// endregion
--- src/oop.js
+++ src/oop.js
@@ -2,110 +2,110 @@
 
 import jQuery from 'node_modules/jquery/dist/jquery';
 
-	function initExtensionsFor (constructor, context, args) {
-		if (constructor === Root) {
-			return;
+function initExtensionsFor (constructor, context, args) {
+	if (constructor === Root) {
+		return;
+	}
+	initExtensionsFor(constructor.superclass, context, args);		// super -> base
+	
+	// call Class constructor
+	if (constructor.prototype.hasOwnProperty('init')) {
+		constructor.prototype.init.apply(context, args);
+	}
+	// call Extension constructor, init
+	constructor.__ext.forEach(function (ext) {
+		ext.apply(context, args);
+		if (ext.prototype.hasOwnProperty('init')) {
+			ext.prototype.init.apply(context, args);
 		}
-		initExtensionsFor(constructor.superclass, context, args);		// super -> base
-		
-		// call Class constructor
-		if (constructor.prototype.hasOwnProperty('init')) {
-			constructor.prototype.init.apply(context, args);
-		}
-		// call Extension constructor, init
-		constructor.__ext.forEach(function (ext) {
-			ext.apply(context, args);
-			if (ext.prototype.hasOwnProperty('init')) {
-				ext.prototype.init.apply(context, args);
+	});
+}
+
+export function Root () {
+	// walk inheritance chain, call each (super) class extensions' init functions
+	initExtensionsFor(this.constructor, this, arguments);
+}
+
+Root.descendsFromRoot = function (constructor) {
+	return constructor === Root || (
+		constructor.superclass && Root.descendsFromRoot(constructor.superclass)
+	);
+};
+
+/**
+ * Build a constructor function
+ * 
+ * The constructor itself will only call its parent. This recursion will
+ * stop with Root(). Extension constructors are called.
+ * 
+ * Each class' and extension's init() function is called. The order is
+ * bottom-up (e.g. sub -> super), and extensions after the classes they
+ * were set up on.
+ * 
+ * Init functions (and the extension constructors) are called with the
+ * arguments used for the built class.
+ * 
+ * Example:
+ *   B = buildClass(Root);
+ *   
+ *   E = function () {}
+ *   E.prototype.init = function () {}
+ *   
+ *   F = buildClass(B, [E], {
+ *     init: function () {}
+ *   });
+ *   
+ *   new F();
+ *   
+ * Call order:
+ *   F() -> B() -> Root()	// Root() in turn will call
+ *   B.init(), F.init(), E(), E.init()
+ *   F's prototype will have E's mixed in
+ * 
+ * @param {Function} base				Must be Root or subclass of Root
+ * @param {[Function]} [extensions]		List of extension constructors
+ * @param {Object} [protoProps]			Properties to merge onto the built class' prototype (e.g. Func.prototype.prop)
+ * @param {Object} [staticProps]		Properties to merge onto the built class (e.g. Func.prop)
+ * @returns {Function}
+ */
+export function buildClass (base, extensions, protoProps, staticProps) {
+	// assert base's prototype chain includes Root
+	if (!Root.descendsFromRoot(base)) {
+		throw 'base class does not descend from Root';
+	}
+	
+	// build constructor
+	function F () {
+		base.apply(this, arguments);			// top-down (super -> base) recursion
+	}
+	
+	F.superclass = base;
+	
+	// set prototype
+	F.prototype = Object.create(base.prototype);
+	F.prototype.constructor = F;
+	
+	// register extensions and merge their prototypes
+	F.__ext = [];
+	if (extensions) {
+		extensions.forEach(function (ext) {
+			var originalConstructor = F.prototype.constructor;
+			
+			F.__ext.push(ext);
+			jQuery.extend(F.prototype, ext.prototype);
+			if (originalConstructor) {
+				F.prototype.constructor = originalConstructor;
 			}
 		});
 	}
 	
-	export function Root () {
-		// walk inheritance chain, call each (super) class extensions' init functions
-		initExtensionsFor(this.constructor, this, arguments);
+	// add properties
+	if (protoProps) {
+		jQuery.extend(F.prototype, protoProps);
+	}
+	if (staticProps) {
+		jQuery.extend(F, staticProps);
 	}
 	
-	Root.descendsFromRoot = function (constructor) {
-		return constructor === Root || (
-			constructor.superclass && Root.descendsFromRoot(constructor.superclass)
-		);
-	};
-	
-	/**
-	 * Build a constructor function
-	 * 
-	 * The constructor itself will only call its parent. This recursion will
-	 * stop with Root(). Extension constructors are called.
-	 * 
-	 * Each class' and extension's init() function is called. The order is
-	 * bottom-up (e.g. sub -> super), and extensions after the classes they
-	 * were set up on.
-	 * 
-	 * Init functions (and the extension constructors) are called with the
-	 * arguments used for the built class.
-	 * 
-	 * Example:
-	 *   B = buildClass(Root);
-	 *   
-	 *   E = function () {}
-	 *   E.prototype.init = function () {}
-	 *   
-	 *   F = buildClass(B, [E], {
-	 *     init: function () {}
-	 *   });
-	 *   
-	 *   new F();
-	 *   
-	 * Call order:
-	 *   F() -> B() -> Root()	// Root() in turn will call
-	 *   B.init(), F.init(), E(), E.init()
-	 *   F's prototype will have E's mixed in
-	 * 
-	 * @param {Function} base				Must be Root or subclass of Root
-	 * @param {[Function]} [extensions]		List of extension constructors
-	 * @param {Object} [protoProps]			Properties to merge onto the built class' prototype (e.g. Func.prototype.prop)
-	 * @param {Object} [staticProps]		Properties to merge onto the built class (e.g. Func.prop)
-	 * @returns {Function}
-	 */
-	export function buildClass (base, extensions, protoProps, staticProps) {
-		// assert base's prototype chain includes Root
-		if (!Root.descendsFromRoot(base)) {
-			throw 'base class does not descend from Root';
-		}
-		
-		// build constructor
-		function F () {
-			base.apply(this, arguments);			// top-down (super -> base) recursion
-		}
-		
-		F.superclass = base;
-		
-		// set prototype
-		F.prototype = Object.create(base.prototype);
-		F.prototype.constructor = F;
-		
-		// register extensions and merge their prototypes
-		F.__ext = [];
-		if (extensions) {
-			extensions.forEach(function (ext) {
-				var originalConstructor = F.prototype.constructor;
-				
-				F.__ext.push(ext);
-				jQuery.extend(F.prototype, ext.prototype);
-				if (originalConstructor) {
-					F.prototype.constructor = originalConstructor;
-				}
-			});
-		}
-		
-		// add properties
-		if (protoProps) {
-			jQuery.extend(F.prototype, protoProps);
-		}
-		if (staticProps) {
-			jQuery.extend(F, staticProps);
-		}
-		
-		return F;
-	}
+	return F;
+}
