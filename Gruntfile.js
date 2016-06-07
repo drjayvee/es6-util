85e3ae8ab6046830a353845ec62d3a445a91ea6e
Jeroen Versteeg <jeroen@asystance.nl>
2016-06-07 15:18:44 +0200

    improved babel config in grunt, added watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,26 +5,22 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 	
 	grunt.initConfig({
+		watch: {
+			files: ['src/**/*.js'], 
+			tasks: ['babel']
+		},
 		babel: {
 			options: {
 				modules: 'amd',
 				sourceMap: true
 			},
 			dist: {
-				files: {
-					'js/oop.js':		'src/js/oop.js',
-					'js/attribute.js':	'src/js/attribute.js',
-					'js/eventTarget.js':'src/js/eventTarget.js',
-					'js/widget.js':		'src/js/widget.js',
-					'js/button.js':		'src/js/button.js',
-					
-					'example/widgets.js': 		'src/example/widgets.js',
-					
-					'test/oop-test.js': 		'src/test/oop-test.js',
-					'test/eventTarget-test.js':	'src/test/eventTarget-test.js',
-					'test/attribute-test.js':	'src/test/attribute-test.js',
-					'test/button-test.js':		'src/test/button-test.js'
-				}
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: ['**/*.js'],
+					dest: '.'
+				}]
 			}
 		},
 		
@@ -33,5 +29,6 @@ module.exports = function(grunt) {
 		}
 	});
 	
+	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.registerTask('default', ['clean:babel', 'babel']);
 };
