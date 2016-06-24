81482a237fd746724afb85fdc278df3d24ed4e26
Jeroen Versteeg <jeroen@asystance.nl>
2016-06-24 14:24:22 +0200

    added two ButtonGroups to example

--- src/example/widgets.js
+++ src/example/widgets.js
@@ -1,8 +1,10 @@
 /*jshint esnext:true*/
 
-import {Button, ToggleButton} from 'js/button';
+import {createButton, createToggleButton} from 'js/button';
+import createButtonGroup from 'js/buttonGroup';
 
-let bt = Button({
+// region Button
+const bt = createButton({
 	label: 'No touching!',
 	disabled: true
 });
@@ -18,3 +20,21 @@ bt.node.addEventListener('click', () => {
 	bt.set('label', 'You got me')
 		.set('disabled', true);
 });
+// endregion
+
+// region ButtonGroup
+const checkGroup = createButtonGroup({
+	children: [
+		createToggleButton({label: 'check', pressed: true}),
+		createToggleButton({label: 'mate'})
+	]
+}).render();
+
+const radioGroup = createButtonGroup({
+	children: [
+		createToggleButton({label: 'blue pil'}),
+		createToggleButton({label: 'red pil'})
+	],
+	radio: true
+}).render();
+// endregion
