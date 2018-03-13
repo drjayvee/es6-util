90a956e580c2bb459f63a25983f90093c9c85285
Jeroen Versteeg <jeroen@asystance.nl>
2018-03-13 16:24:41 +0100

    implemented panel features: no close button, closeOn*, modality

--- src/js/panel.js
+++ src/js/panel.js
@@ -5,6 +5,18 @@ import createOverlay from 'js/overlay';
 
 const CLOSE_ICON = '<span class="icon icon-close hasrole"></span>';
 
+const MODAL_MASK = '<div class="yui3-widget-mask"></div>';
+
+let modalMask = null;
+function modality (enable = true) {
+	if (enable && !modalMask) {
+		document.body.insertAdjacentHTML('afterbegin', MODAL_MASK);
+		modalMask = document.body.firstElementChild;
+	}
+	
+	modalMask.hidden = !enable;
+}
+
 /**
  * @class Panel
  * @augments Overlay
@@ -13,7 +25,11 @@ const CLOSE_ICON = '<span class="icon icon-close hasrole"></span>';
 
 /**
  * @typedef {Object} PanelConfig
- * @property {Button[]} [buttons]
+ * @property {Button[]} [buttons=[]]
+ * @property {Boolean} [closeButton=true]
+ * @property {Boolean} [closeOnEsc=true]
+ * @property {Boolean} [closeOnClickOutside=true]
+ * @property {Boolean} [modal=false]
  * @see WidgetConfig
  */
 
@@ -38,9 +54,11 @@ const createPanel  = createOverlay.extend(/** @lends Panel.prototype */{
 		createOverlay.prototype._render.apply(this, arguments);
 		
 		// close button
-		this.node.firstElementChild.classList.add('drag-handle');
-		this.node.firstElementChild.insertAdjacentHTML('beforeend', CLOSE_ICON);
-		this.node.firstElementChild.lastElementChild.addEventListener('click', () => this.hide());
+		if (this._closeButton) {
+			this.node.firstElementChild.classList.add('drag-handle');
+			this.node.firstElementChild.insertAdjacentHTML('beforeend', CLOSE_ICON);
+			this.node.firstElementChild.lastElementChild.addEventListener('click', () => this.hide());
+		}
 		
 		// footer buttons
 		for (let button of this.get('buttons')) {
@@ -53,10 +71,53 @@ const createPanel  = createOverlay.extend(/** @lends Panel.prototype */{
 		this.node.lastElementChild.hidden = false;
 	},
 	
+	_setContent () {
+		if (!this._closeButton || !this.get('rendered')) {		// don't bother on initial render
+			createOverlay.prototype._setContent.apply(this, arguments);
+			return;
+		}
+		
+		const headerNode = this.node.firstElementChild;
+		const closeButton = headerNode.lastElementChild;
+		
+		headerNode.removeChild(closeButton);
+		createOverlay.prototype._setContent.apply(this, arguments);
+		headerNode.appendChild(closeButton);
+	},
+	
 	enableDragging (handle = '.yui3-widget-hd', container = null, padding = 0) {
-		createOverlay.prototype.enableDragging.call(this, handle || '.yui3-widget-hd', container, padding);
+		return createOverlay.prototype.enableDragging.call(this, handle || '.yui3-widget-hd', container, padding);
 	}
 	
+}, function init (superInit, {closeButton = true, closeOnEsc = true, closeOnClickOutside = true, modal = false} = {}) {
+	superInit();
+	
+	this._closeButton = closeButton;
+	
+	this.onceAttrVal('rendered', true, () => {
+		if (closeOnEsc) {
+			document.addEventListener('keydown', e => {
+				if (e.keyCode === 27 && this.get('visible')) {
+					this.hide();
+				}
+			});
+		}
+		
+		if (closeOnClickOutside) {
+			// TODO: use util.js onClickOutside
+			const col = e => {
+				if (e.keyCode === 27 && this.get('visible')) {
+					this.hide();
+					document.removeEventListener('click', col);
+				}
+			};
+			document.addEventListener('click', col);
+		}
+	});
+	
+	if (modal) {
+		this.after('renderedChange', e => modality(e.newVal));
+	}
 });
 
 export default createPanel;
