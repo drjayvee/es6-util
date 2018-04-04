d524fe2640616c3cc4b01bed87f59d84198a9957
Jeroen Versteeg <jeroen@asystance.nl>
2018-04-04 09:43:18 +0200

    implemented basic drag&drop in tree

--- src/js/tree.js
+++ src/js/tree.js
@@ -5,11 +5,86 @@ import {createAttributeObservable} from 'js/attribute';
 const h = maquette.h;
 const projector = maquette.createProjector();
 
+const map = new Map();
+
+
+
+// region drag & drop
+function encodeDnDItem (e) {
+	const item = e.target.bind;
+	
+	e.dataTransfer.setData('text/plain', JSON.stringify([
+		map.get(item.getRoot()),						// root's DOMElement id
+		createLeaf.created(item) ? 'leaf' : 'node',		// item type
+		item.get('id')									// item id
+	]));
+}
+
+function decodeDnDItem (e) {
+	const [rootElementId, itemType, itemId] = JSON.parse(e.dataTransfer.getData('text/plain'));
+	
+	let tree;
+	for (let [t, el] of map.entries()) {
+		if (el === rootElementId) {
+			tree = t;
+			break;
+		}
+	}
+	
+	if (!tree) {
+		return null;
+	}
+	
+	return tree.getItem(itemId, itemType === 'leaf' ? createLeaf : createNode);
+}
+
+function isValidDropTarget (e) {
+	const dragItem = decodeDnDItem(e);
+	const dropTarget = e.target;
+	const dropItem = dropTarget.bind || dropTarget.parentNode.bind;
+	
+	return !(
+		dropTarget.classList.contains('leaf')						||	// target is leaf item
+		dragItem === dropItem										||	// target is self
+		dragItem.parent === dropItem								||	// current parent
+		createNode.created(dragItem) && dragItem.contains(dropItem)	||	// target is descendant
+		dragItem.getRoot() !== dropItem.getRoot()						// target is in other tree
+	);
+}
+
+window.treeDnD_start = e => {
+	e.dataTransfer.effectAllowed = 'move';
+	
+	encodeDnDItem(e);
+};
+
+window.treeDnD_over = e => {
+	e.preventDefault();
+	
+	e.dataTransfer.dropEffect = isValidDropTarget(e) ? 'move' : 'none';
+};
+
+window.treeDnD_drop = e => {
+	e.preventDefault();
+	e.stopPropagation();	// don't bubble up to parent nodes
+	
+	if (!isValidDropTarget(e)) {
+		return;
+	}
+	
+	const draggedItem = decodeDnDItem(e);
+	const dragTarget = (e.target.classList.contains('node') ? e.target : e.target.parentNode).bind;
+	
+	draggedItem.moveTo(dragTarget);
+};
+// endregion
+
 
 
 /**
  * @class Item
  * @augments AttributeObservable
+ * @property {Node} [parent]
  */
 
 /**
@@ -25,11 +100,37 @@ const projector = maquette.createProjector();
  * @param {ItemConfig} config
  * @return {Item}
  */
-const createItem = createAttributeObservable.extend({
+const createItem = createAttributeObservable.extend(/** @lends Item.prototype */{
 	
 	ATTRS: {
 		id: {},
 		label: {},
+	},
+
+	/**
+	 * 
+	 * @return {Node}
+	 */
+	getRoot () {
+		return this.parent.getRoot();
+	},
+
+	/**
+	 * 
+	 * @param {Node} newParent
+	 */
+	moveTo (newParent) {
+		if (newParent === this.parent) {
+			return;
+		}
+		
+		this.parent.removeItem(this);
+		newParent.addItem(this);
+		this.parent = newParent;
+	},
+	
+	toString () {	// for sort()ing
+		return (createLeaf.created(this) ? '1' : '0') + this.get('label');
 	},
 	
 });
@@ -48,7 +149,11 @@ const createLeaf = createItem.extend({
 	
 	_render () {
 		return h('li.leaf', {
-			key: this.get('id'),	// TODO: type + id
+			bind: this,
+			'data-leaf-id': String(this.get('id')),
+			
+			draggable: 'true',
+			ondragstart: 'treeDnD_start(event)',
 		}, [
 			this.get('label')
 		]);
@@ -73,7 +178,7 @@ const createLeaf = createItem.extend({
  * @param {NodeConfig} config
  * @return {Node}
  */
-const createNode = createItem.extend({
+const createNode = createItem.extend(/** @lends Node.prototype */{
 	
 	ATTRS: {
 		expanded: {
@@ -86,6 +191,7 @@ const createNode = createItem.extend({
 				return children.map(child => {
 					if (!createItem.created(child)) {
 						child = child.type === 'node' ? createNode(child): createLeaf(child);
+						child.parent = this;
 						child.addBubbleTarget(this);
 					}
 					return child;
@@ -96,53 +202,93 @@ const createNode = createItem.extend({
 
 	/**
 	 * 
-	 * @param {ItemConfig|Item} config
+	 * @param {ItemConfig|Item} item
 	 */
-	addItem (config) {
+	addItem (item) {
 		let children = this.get('children');
-		// TODO: sort!
-		children.push(config);
+		children.push(item);
 		this.set('children', children);
 	},
 	
+	/**
+	 * 
+	 * @param {Item} item
+	 */
+	removeItem (item) {
+		let children = this.get('children');
+		children = children.filter(child => child !== item);
+		this.set('children', children);
+	},
+
+	/**
+	 * 
+	 * @param {*} id
+	 * @return {Leaf}
+	 */
 	getLeaf (id) {
-		return this._recursiveFind(id, createLeaf);
+		return this.getItem(id, createLeaf);
 	},
 	
+	/**
+	 * 
+	 * @param {*} id
+	 * @return {Node}
+	 */
 	getNode (id) {
-		return this._recursiveFind(id, createNode);
+		return this.getItem(id, createNode);
 	},
 	
-	_recursiveFind (id, factory) {
+	/**
+	 * 
+	 * @param {*} id
+	 * @param {Function} factory
+	 * @return {Item}
+	 */
+	getItem (id, factory) {
 		for (let child of this.get('children')) {
-			if (!factory.created(child)) {
-				continue;
-			}
-			
-			if (child.get('id') === id) {
+			if (factory.created(child) && child.get('id') === id) {
 				return child;
 			}
 			
-			const descendantNode = child.getNode(id);
+			const descendantNode = child.getItem && child.getItem(id, factory);
 			if (descendantNode) {
 				return descendantNode;
 			}
 		}
 		return null;
 	},
+
+	/**
+	 * @param {Item} item
+	 * @return bool
+	 */
+	contains (item) {
+		return this.getItem(
+			item.get('id'),
+			createLeaf.created(item) ? createLeaf : createNode
+		);
+	},
+	
+	toggle (expanded = !this.get('expanded')) {
+		this.set('expanded', expanded);
+	},
 	
 	_render () {
-		const expanded = this.get('expanded');
-		const children = this.get('children');
-		
 		return h(
 			'li.node',
 			{
-				key: this.get('id'),
+				bind: this,
+				'data-node-id': String(this.get('id')),
+				
 				classes: {
-					expanded,
-					empty: !children.length
-				}
+					expanded:	this.get('expanded'),
+					empty:		!this.get('children').length
+				},
+				
+				draggable: 'true',
+				ondragstart:'treeDnD_start(event)',
+				ondragover:	'treeDnD_over(event)',
+				ondrop:		'treeDnD_drop(event)'
 			},
 			[
 				h('span.label', [this.get('label')])
@@ -161,7 +307,7 @@ const createNode = createItem.extend({
 		return h(
 			'ul',
 			className ? {class: className} : {},
-			this.get('children').map(item => item._render())
+			this.get('children').sort().map(item => item._render())
 		);
 	}
 });
@@ -173,6 +319,10 @@ const createNode = createItem.extend({
  */
 const createTree = createNode.extend({
 
+	getRoot () {
+		return this;
+	},
+	
 	/**
 	 * @override
 	 */
@@ -182,6 +332,11 @@ const createTree = createNode.extend({
 	
 }, function init (superInit, items, parentNode) {
 	superInit();
+	
+	if (!parentNode.id) {
+		parentNode.id = 'tree' + map.size;
+	}
+	map.set(this, parentNode.id);
 	
 	this.set('expanded', true);
 	this.set('children', items);
@@ -194,6 +349,19 @@ const createTree = createNode.extend({
 	this.after('labelChange', render);
 	this.after('expandedChange', render);
 	this.after('childrenChange', render);
+	
+	// attach event listeners
+	parentNode.addEventListener('click', e => {
+		let ie = e.target;
+		if (!ie.classList.contains('leaf')) {
+			ie = ie.parentNode;
+		}
+		const item = ie.bind;
+		
+		if (createNode.created(item)) {
+			item.toggle();
+		}
+	});
 });
 
 export default createTree;
